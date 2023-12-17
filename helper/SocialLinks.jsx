6503d12b562b0cfc6e572a848c9b7580bad59c9e
victordanalyst <viccrowns@gmail.com>
2023-12-17 21:20:43 +0100

    fixed

--- helper/SocialLinks.jsx
+++ helper/SocialLinks.jsx
@@ -8,19 +8,19 @@ export default function SocialLinks() {
     <section className="absolute left-[50%] top-[25%] lg:top-[90%] lg:left-[90%] -translate-x-[50%] -translate-y-[50%]">
       <div className="flex items-center gap-4 lg:gap-2">
         <Link
-          href="https://twitter.com/drealmlex/"
+          href="https://twitter.com/devmarleyy/"
           className="border-[1px] border-gray-800 lg:border-0 p-2 hover:bg-white rounded-[50%] SocialTransition SocialShadow"
         >
           <AiOutlineTwitter size={22} className="text-gray-800" />
         </Link>
         <Link
-          href="https://www.instagram.com/drealmlex/"
+          href=""
           className="border-[1px] border-gray-800 lg:border-0 p-2 hover:bg-white rounded-[50%] SocialTransition SocialShadow"
         >
           <AiOutlineInstagram size={22} className="text-gray-800" />
         </Link>
         <Link
-          href="http://linkedin.com/in/gabriel-dream-043268247"
+          href="http://linkedin.com/in/ugochukwu victor"
           className="border-[1px] border-gray-800 lg:border-0 p-2 hover:bg-white rounded-[50%] SocialTransition SocialShadow"
         >
           <BiLogoLinkedin size={22} className="text-gray-800" />
